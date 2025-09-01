4ca7e19e04a70c0ee5b93960a44817a9e7825e65
youngishcord <kulikobyura@mail.ru>
2025-09-02 00:21:22 +0300

    dev: реализован примитивный ответ
    Необходимо посмотреть как должна происходить работа с очередью.

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -14,16 +16,44 @@ func failOnError(err error, msg string) {
 	}
 }
 
+// CeleryTask Структура задачи Celery (упрощённая)
+type CeleryTask struct {
+	ID      string        `json:"id"`
+	Task    string        `json:"task"`
+	Args    []interface{} `json:"args"`
+	Kwargs  interface{}   `json:"kwargs"`
+	ReplyTo string        `json:"reply_to"`
+}
+
+// CeleryResult Структура результата (для backend rpc)
+type CeleryResult struct {
+	Status string      `json:"status"`
+	Result interface{} `json:"result"`
+	Trace  string      `json:"traceback"`
+	TaskID string      `json:"task_id"`
+}
+
 func main() {
 	fmt.Println("Begin")
 
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5545/")
 	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+	defer func(conn *amqp.Connection) {
+		err := conn.Close()
+		if err != nil {
+
+		}
+	}(conn)
 
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
+	defer func(ch *amqp.Channel) {
+		err := ch.Close()
+		if err != nil {
+
+		}
+	}(ch)
+
 	// q, err := ch.QueueDeclare(
 	// 	"celery", // name
 	// 	false,    // durable
@@ -35,21 +65,76 @@ func main() {
 	// failOnError(err, "Fail on declare a queue")
 
 	msgs, err := ch.Consume(
-		"celery", // queue
-		"",       // consumer
-		true,     // auto-ack
-		false,    // exclusive
-		false,    // no-local
-		false,    // no-wait
-		nil,      // args
+		// Указание очереди
+		"qwer",       // queue
+		"consumer_1", // consumer
+		false,        // auto-ack
+		false,        // exclusive
+		false,        // no-local
+		false,        // no-wait
+		nil,          // args
 	)
 	failOnError(err, "Failed to register a consumer")
 
-	for d := range msgs {
+	for message := range msgs {
+		fmt.Println("Received a message:", message)
+		fmt.Println("Received a message body:", string(message.Body))
+		//fmt.Println("Received a message task:", string(message.))
 
-		fmt.Println(
-		// json.Unmarshal(d.Body),
-		)
+		var raw []interface{}
+		if err := json.Unmarshal(message.Body, &raw); err != nil {
+			log.Fatal(err)
+			//err := message.Nack(false, true)
+			//if err != nil {
+			//	return
+			//}
+			//continue
+		}
+		fmt.Println(raw)
+		fmt.Println(raw[0])
+
+		//var tr CeleryTask
+		//err := json.Unmarshal(message.Body, &tr)
+		//if err != nil {
+		//	log.Printf("Error unmarshalling task result: %s", err)
+		//}
+		//fmt.Println(tr)
+		//
+		res := CeleryResult{
+			Status: "SUCCESS",
+			Result: 123,
+			Trace:  "",
+			TaskID: message.CorrelationId,
+		}
+
+		body, err := json.Marshal(res)
+		if err != nil {
+			log.Printf("Error marshalling task result: %s", err)
+		}
+
+		// Слип для проверки работы временный ack очередей
+		time.Sleep(15 * time.Second)
+
+		if message.ReplyTo != "" {
+			err = ch.PublishWithContext(
+				context.Background(),
+				"",
+				message.ReplyTo,
+				false,
+				false,
+				amqp.Publishing{
+					ContentType:   "application/json",
+					CorrelationId: message.CorrelationId,
+					Body:          body,
+				},
+			)
+		}
+		if err != nil {
+			log.Printf("Error publishing task result: %s", err)
+		}
+		err = message.Ack(false)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-
 }
--- py/main.py
+++ py/main.py
@@ -1,7 +1,6 @@
 import datetime
-import time
 import celery
-
+from celery import Task
 
 app = celery.Celery(
 	"publisher",
@@ -9,17 +8,26 @@ app = celery.Celery(
 	backend="rpc://"
 )
 
+class CustomTask(Task):
+    def __init__(self, name, *args, **kwargs):
+        super(Task, self).__init__(*args, **kwargs)
+        self.name = name
+
+@app.task(name="add", queue="qwer")
+def add(x, y):
+    return x + y
 
 def pub_message():
-	task = celery.Task()
-	# task.set(queue="echo")
-	while 1:
-		# to default queue "celery"
-		res = task.delay({"message": "this is message", "time": datetime.datetime.now(), "sleep_time": 1})
-		print(res)
-		print("on sleep")
-		time.sleep(10)
+    custom_task = CustomTask("test_task").s().set(queue="qwer")
 
+    # res = add.delay(1, 2)
+    # print(res.get())
+
+    res = custom_task.delay({"message": "this is message", "time": datetime.datetime.now(), "sleep_time": 1})#.get()
+    print("Задача отправлена")
+    print(res)
+    print("Встала на ожидание")
+    print(res.get())
 
 if __name__ == "__main__":
-	pub_message()
+    pub_message()
